00799ad768fea86f89ee15a7788fcc57f226dfbb
paul-talma <109061871+paul-talma@users.noreply.github.com>
2025-12-02 19:45:48 -0800

    2025 days 1 and 2

---
+++ 2025/day01/day01.py
@@ -0,0 +1,53 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+from utils import utils
+
+INPUT = utils.read_file('input.txt')
+INPUT = [(line[0], int(line[1:])) for line in INPUT]
+
+
+def part1():
+    INITIAL = 50
+    MAX = 100
+    SIGN_DICT = {'R': 1, 'L': -1}
+    val = INITIAL
+    counter = 0
+    for instruction in INPUT:
+        dir = instruction[0]
+        sign = SIGN_DICT[dir]
+        val += sign * instruction[1]
+        val = val % MAX
+        if val == 0:
+            counter += 1
+    print(f'The code is: {counter}')
+
+
+def part2():
+    INITIAL = 50
+    MAX = 100
+    SIGN_DICT = {'R': 1, 'L': -1}
+    val = INITIAL
+    counter = 0
+    # __import__('ipdb').set_trace()
+    for instruction in INPUT:
+        dir = instruction[0]
+        sign = SIGN_DICT[dir]
+        displacement = instruction[1]
+        if dir == 'L':
+            val = (MAX - val) % MAX
+        val += displacement
+        turns, val = divmod(val, MAX)
+        turns = abs(turns)
+        if turns == 0:
+            counter += val == 0
+        else:
+            counter += turns
+        if dir == 'L':
+            val = (MAX - val) % MAX
+    print(f'The code is: {counter}')
+
+
+part2()
---
+++ 2025/day02/day02.py
@@ -0,0 +1,30 @@
+import re
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+from utils import utils
+
+INPUT = utils.read_input_sequence('input.txt')
+
+pattern = r'(\d+)\1+'
+
+
+def match(id):
+    str_id = str(id)
+    return re.fullmatch(pattern, str_id)
+
+
+def part1():
+    total = 0
+    for r in INPUT:
+        lower, upper = r.split('-')
+        for id in range(int(lower), int(upper) + 1):
+            if match(id):
+                total += id
+
+    print(f'Total: {total}')
+
+
+part1()
